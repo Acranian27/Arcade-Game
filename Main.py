b992986ed4b5913629823abc9b49b8c42b73775b
Andrew Kerr <andrewkerr@MacBookAir.lan>
2026-03-22 00:58:17 +0800

    Update 1.1: Created the foundation for the Main Menu logic

--- Main.py
+++ Main.py
@@ -10,3 +10,34 @@ Host the main logic of the program.
 - Run forever unless the user inputs as such
 
 """
+
+from InputValidatorModule import UserInput # Call a function to retrieve a valid user input
+from UIModule import MainMenuButtons # Call a function to output the main menu options
+
+def Main():
+    while True:
+        MainMenuButtons()
+        Choice = UserInput("MainMenu")
+
+        # Those with "pass" will have modules called  once the modules are created
+        if Choice == "W":
+            print("You've chosen Wordle, lets begin!")
+            pass
+
+        elif Choice == "C":
+            print("You've chosen Connections, lets begin!")
+            pass
+
+        elif Choice == "H":
+            print("You've chosen to ask for help.")
+            pass
+
+        elif Choice == "S":
+            print("You've chosen to view your scores.")
+            pass
+
+        elif Choice == "Q":
+            print("You've chosen to quit, hope you had fun!\nBye for now...")
+            break
+
+Main()
