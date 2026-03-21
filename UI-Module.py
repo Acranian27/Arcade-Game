5b2ac912bbd8a1058ad7422f3e4bf05c2a0f058c
Andrew Kerr <andrewkerr@MacBookAir.lan>
2026-03-22 00:38:00 +0800

    Update 1.1: Created an output for the Main Menu

--- UI-Module.py
+++ UI-Module.py
@@ -1,8 +1,30 @@
 """
 # Purpose:
-This is meant to host the main UI for the program.
+This is meant to host the UI for the program.
 
 # To Do:
-- Output the buttons for the main menu
+- Output the buttons for the main menu -> COMPLETE
+- Output the information for 'Help Menu' and 'Show Scores'
 
 """
+
+def OutputButtons(Stage):
+
+    if Stage == "Main":
+
+        print(
+            "- - - - - - - - - - - - -"
+            "\n« Wordle [W] »\n"
+            "- - - - - - - - - - - - -"
+            "\n« Connections [C] »\n"
+            "- - - - - - - - - - - - -"
+            "\n« Help Menu [H] »\n"
+            "- - - - - - - - - - - - -"
+            "\n« Show Scores [S] »\n"
+            "- - - - - - - - - - - - -"
+            "\n« Quit Program [Q] »\n"
+            "- - - - - - - - - - - - -"
+        )
+
+    print("\nSelect any of the options above by inputting the keybind in the square brackets."
+          "\ne.g. for « Wordle [W] », input 'W'\n")
