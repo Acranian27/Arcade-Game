9e89c7605d448056c13f55c517ac794322a4cac8
Andrew Kerr <andrewkerr@MacBookAir.lan>
2026-03-22 18:53:38 +0800

    Update 3: Created the core logic and UI for Wordle

--- UIModule.py
+++ UIModule.py
@@ -4,9 +4,17 @@ This is meant to host the UI for the program.
 
 # To Do:
 - Output the buttons for the main menu -> COMPLETE
+- Output the UI for Wordle -> COMPLETE
+- Output the UI for Connections
 - Output the information for 'Help Menu' and 'Show Scores'
 
 """
+
+# Colours:
+ResetColour = "\033[0m"
+GreenBackground = "\033[42m\033[1;38m"
+YellowBackground = "\033[43m\033[1;38m"
+GreyBackground = "\033[47m\033[1;38m"
 
 def MainMenuButtons():
 
@@ -27,3 +35,24 @@ def MainMenuButtons():
     print("\nSelect any of the options above by inputting the keybind in the square brackets."
           "\ne.g. for « Wordle [W] », input 'W'\n")
 
+# Outputs the colouring of the letter based on it's correctness
+
+PreviousGuesses = [" -  -  -  -  - \n"] * 6
+
+def WordleUI(Guess, States, Attempt):
+
+    CurrentWord = []
+
+    # Gives each letter a unique colour based on their state
+    for i, letter in enumerate(Guess):
+        if States[i] == "green":
+            CurrentWord.append(f"{GreenBackground} {letter} {ResetColour}")
+
+        elif States[i] == "yellow":
+            CurrentWord.append(f"{YellowBackground} {letter} {ResetColour}")
+
+        else:
+            CurrentWord.append(f"{GreyBackground} {letter} {ResetColour}")
+
+    PreviousGuesses[Attempt - 1] = f'{"".join(CurrentWord)}\n'
+    print("".join(PreviousGuesses))
--- WordleModule.py
+++ WordleModule.py
@@ -3,14 +3,16 @@
 This is meant to host the Wordle game logic and processing necessary to run the game.
 
 # To Do:
-- Select a random word the user needs to guess
-- Retrieve a user input
-- Compare the user input to the random word
-- Run this module until the user guesses correctly or after 6 incorrect attempts
+- Select a random word the user needs to guess -> COMPLETE
+- Retrieve a user input -> COMPLETE
+- Compare the user input to the random word -> COMPLETE
+- Run this module until the user guesses correctly or after 6 incorrect attempts -> COMPLETE
 - Get a score based on this
-- Return to the main module
 
 """
+import random
+from InputValidatorModule import UserInput
+from UIModule import WordleUI
 
 # Gathers the list of possible words from WordleData.txt
 with open("WordleData.txt", "r") as file:
@@ -24,3 +26,61 @@ with open("WordleData.txt", "r") as file:
 
         word = word.strip() # Remove unnecessary spaces
         CleanList.append(word)
+
+# Chooses a random word from the list
+def SelectRandomWord():
+
+    index = random.randint(0, len(CleanList) - 1) # Python starts at zero not one
+
+    return CleanList[index]
+
+# Adds a state for each letter in Guess inside a list
+def DetermineStates(Answer, Guess):
+
+    LetterFrequency = {}
+    LetterStates = []
+
+    # Used to show the correct number of letters of the same type
+    for letter in Answer:
+        if letter in LetterFrequency:
+            LetterFrequency[letter] += 1
+        else:
+            LetterFrequency[letter] = 1 # It must be defined before 1 can be added
+
+    # Calculates the states for each letter
+    for pos, letter in enumerate(Guess):
+        if letter == Answer[pos]:
+            LetterStates.append("green")
+            LetterFrequency[letter] -= 1
+
+        elif letter in Answer and LetterFrequency[letter] > 0:
+            LetterStates.append("yellow")
+            LetterFrequency[letter] -= 1
+
+        else:
+            LetterStates.append("grey")
+
+    return LetterStates
+
+# Calls the core functions to run Wordle
+def WordleMain():
+
+    RandomWord = SelectRandomWord()
+    Attempts = 0
+    Correct = False
+
+    while Attempts < 6 and not Correct:
+
+        Guess = UserInput("Wordle")
+        States = DetermineStates(RandomWord, Guess)
+
+        Attempts += 1
+        WordleUI(Guess, States, Attempts)
+
+        if Guess == RandomWord:
+            Correct = True
+
+    if Correct:
+        print("Congratulations! You guessed the word correctly!")
+    else:
+        print("Tough luck, the word was", RandomWord)
